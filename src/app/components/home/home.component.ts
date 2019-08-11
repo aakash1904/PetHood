e5b9e1a8bac258c579395f678c3d22b42997587c
TamanpreetSingh <tamanpreet20@gmail.com>
2019-08-11 15:33:23 +0530

    Adding responsiveness

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  windowWidth: number = window.innerWidth;
+  hidden : boolean;
+  constructor() { 
+    this.hidden=false;
+  }
+  ngAfterViewInit() {
+    this.windowWidth = window.innerWidth;
+}
+@HostListener('window:resize', ['$event'])
+  resize(event) {
+      this.windowWidth = window.innerWidth;
+  }
   ngOnInit() {
   }
 
